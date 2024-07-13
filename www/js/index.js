2837fdc15e29ce672d6d2562bf92d86fbbed5165
Ezequiel <95730033+EzequielSOliveira@users.noreply.github.com>
2024-07-13 08:21:25 -0300

    feat: criado estrutura inicial das principais páginas do site, incluíndo menu principal e rodapé.

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,17 @@
 ﻿/**
  * 
- * index.js
+ * OPEN NAVIGATION FULL CONTENT
  * 
  */
 
-console.log("Working...")
+/* =================================== OPEN NAVIGATION FULL CONTENT =================================== */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("toogle-navigation-full-content").addEventListener("click", function (event) {
+        this.parentElement.nextElementSibling.classList.toggle("hide");
+    });
+
+});
+
+/* =================================== /OPEN NAVIGATION FULL CONTENT =================================== */
